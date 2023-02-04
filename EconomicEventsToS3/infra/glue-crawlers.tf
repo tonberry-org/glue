3357df5397509f999d05f9e889817ffd6edec5fd
Matthew Tondreau <mmtondreau@gmail.com>
2023-02-04 14:07:47 -0800

    ADd economic events infra

--- EconomicEventsToS3/infra/glue-crawlers.tf
+++ EconomicEventsToS3/infra/glue-crawlers.tf
@@ -1,7 +1,7 @@
 
 resource "aws_glue_crawler" "economic_events" {
   database_name = aws_glue_catalog_database.economic_events.name
-  name          = "NewsCrawler"
+  name          = "EconomicEventsCrawler"
   role          = data.aws_iam_role.glue_general_purpose.arn
   schedule      = "cron(00 09 * * ? *)"
   dynamodb_target {
