b7274aae82b09dccf44d8e4088033517ad2a5a63
Matthew Tondreau <mmtondreau@gmail.com>
2023-02-04 12:35:24 -0800

    Add bonds

--- BondsToS3/infra/glue-crawlers.tf
+++ BondsToS3/infra/glue-crawlers.tf
@@ -1,7 +1,7 @@
 
 resource "aws_glue_crawler" "bonds" {
   database_name = aws_glue_catalog_database.bonds.name
-  name          = "NewsCrawler"
+  name          = "BondsCrawler"
   role          = data.aws_iam_role.glue_general_purpose.arn
   schedule      = "cron(00 09 * * ? *)"
   dynamodb_target {
