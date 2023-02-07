ce4d1a218ec97bd3b1b85a737db427138e4678a6
Matthew Tondreau <mmtondreau@gmail.com>
2023-02-06 19:47:21 -0800

    wip

--- NewsToS3/src/NewsToS3.py
+++ NewsToS3/src/NewsToS3.py
@@ -10,35 +10,38 @@ import boto3
 from pyspark.sql.types import Row
 from pyspark.sql.functions import concat_ws, col, coalesce, to_date, split
 
+
 class DDBDelete:
     def __init__(self, table: str, keyGen: Callable[[Row], Dict[str, str]]) -> None:
         self.table = table
         self.keyGen = keyGen
-        
+
     def process(self, df: DynamicFrame) -> None:
         df.toDF().foreachPartition(self.delete)
-        
+
     def delete(self, rows: Iterator[Row]) -> None:
         ddb_underlying_table = boto3.resource("dynamodb").Table(self.table)
         with ddb_underlying_table.batch_writer() as batch:
             for row in rows:
                 batch.delete_item(Key=self.keyGen(row))
 
+
 def init() -> Tuple[GlueContext, Job]:
     params = []
-    if '--JOB_NAME' in sys.argv:
-        params.append('JOB_NAME')
+    if "--JOB_NAME" in sys.argv:
+        params.append("JOB_NAME")
     args = getResolvedOptions(sys.argv, params)
 
     context = GlueContext(SparkContext.getOrCreate())
     job = Job(context)
 
-    if 'JOB_NAME' in args:
-        jobname = args['JOB_NAME']
+    if "JOB_NAME" in args:
+        jobname = args["JOB_NAME"]
     else:
         jobname = "test"
     job.init(jobname, args)
     return (context, job)
+
 
 context, job = init()
 
@@ -49,32 +52,46 @@ news_dynamodb_node: DynamicFrame = context.create_dynamic_frame.from_catalog(
 )
 
 
-transform_node = news_dynamodb_node.toDF().withColumn('tags', concat_ws(',', col('tags')))
-transform_node = transform_node.withColumn('symbols', concat_ws(',', col('symbols')))
-transform_node = transform_node.withColumn('neg_sentiment', coalesce(col('sentiment.neg.long'), col('sentiment.neg.double')))
-transform_node = transform_node.withColumn('pos_sentiment', coalesce(col('sentiment.pos.long'), col('sentiment.pos.double')))
-transform_node = transform_node.withColumn('new_sentiment', coalesce(col('sentiment.neu.long'), col('sentiment.neu.double')))
-transform_node = transform_node.withColumn('publish_timestamp',transform_node['date'])
-transform_node = transform_node.withColumn('date', to_date(transform_node['date']))
-transform_node = transform_node.withColumn('symbol', split(transform_node['symbol:link'], '#').getItem(0))
+transform_node = news_dynamodb_node.toDF().withColumn(
+    "tags", concat_ws(",", col("tags"))
+)
+transform_node = transform_node.withColumn("symbols", concat_ws(",", col("symbols")))
+transform_node = transform_node.withColumn(
+    "neg_sentiment", coalesce(col("sentiment.neg.long"), col("sentiment.neg.double"))
+)
+transform_node = transform_node.withColumn(
+    "pos_sentiment", coalesce(col("sentiment.pos.long"), col("sentiment.pos.double"))
+)
+transform_node = transform_node.withColumn(
+    "new_sentiment", coalesce(col("sentiment.neu.long"), col("sentiment.neu.double"))
+)
+transform_node = transform_node.withColumn("publish_timestamp", transform_node["date"])
+transform_node = transform_node.withColumn("date", to_date(transform_node["date"]))
+transform_node = transform_node.withColumn(
+    "symbol", split(transform_node["symbol:link"], "#").getItem(0)
+)
 
-drop_node = DynamicFrame.fromDF(transform_node.drop('sentiment'), context, "drop_node")
+drop_node = DynamicFrame.fromDF(transform_node.drop("sentiment"), context, "drop_node")
 
 partitioned_dataframe: DynamicFrame = drop_node.toDF().repartition(1)
-partitioned_dynamicframe: DynamicFrame = DynamicFrame.fromDF(partitioned_dataframe, context, "partitioned_df")
+partitioned_dynamicframe: DynamicFrame = DynamicFrame.fromDF(
+    partitioned_dataframe, context, "partitioned_df"
+)
 
 context.write_dynamic_frame.from_options(
     frame=drop_node,
     connection_type="s3",
-    format="csv",
+    format="parquet",
     connection_options={
         "path": "s3://tonberry-news-staging",
         "partitionKeys": ["symbol", "date"],
+        "compression": "gzip",
     },
     transformation_ctx="S3bucket_node3",
 )
 
-DDBDelete("news", lambda x: {"date": x['date'], "symbol:link": x['symbol:link'] }).process(news_dynamodb_node)
+DDBDelete(
+    "news", lambda x: {"date": x["date"], "symbol:link": x["symbol:link"]}
+).process(news_dynamodb_node)
 
 job.commit()
-
